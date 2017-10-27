a8f29764b1344a79fadcc9bfcb2aa4242c8cca4b
Shiyas <shiyas@jxt.com.au>
2017-10-27 15:06:56 +1100

    code baseline

---
+++ cms/globalSetting/globalSetting.js
@@ -0,0 +1,33 @@
+<meta charset="UTF-8" />
+<meta name="msvalidate.01" content="BACBF8B54575E49109D3B04FFF407750" />
+<meta name="google-site-verification" content="k7Dq1Hxs_7Js-wRw9A8jkg6yluB3KtK8BSrt34VZBys" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+<meta property="image" content="/media/aquissearch/images/logo-og.png" />
+<link rel="image_src" href="/media/aquissearch/images/logo-og.png" />
+<link rel="shortcut icon" href="/media/aquissearch/images/favicon.ico" />
+<link rel="apple-touch-icon" href="/media/aquissearch/images/favicon.png"/>  
+<link rel="apple-touch-icon" sizes="72x72" href="/media/aquissearch/images/favicon.png"/>  
+<link rel="apple-touch-icon" sizes="114x114" href="/media/aquissearch/images/favicon.png"/>  
+
+<link rel="stylesheet" href="/fonts/font-awesome/font-awesome.css">
+<link rel="stylesheet" href="/media/aquissearch/css/r15-icons.css" />
+<link rel="stylesheet" href="/media/aquissearch/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="/media/aquissearch/css/style.css?v=1.0" />
+<link rel="stylesheet" href="/media/aquissearch/css/system-responsive.css?v1" />
+
+<!--[if lt IE 9]>
+	<script src="/media/aquissearch/js/html5shiv.js" type="text/javascript"></script>
+	<script src="/media/aquissearch/js/respond.min.js" type="text/javascript"></script>
+<![endif]-->
+
+<script type='text/javascript' src='//www.geolify.com/georedirect.php?id=8838'></script>
+<script src='/media/aquissearch/js/bootstrap.min.js'></script>
+<script src='/scripts/JXTWidget.js'></script> <!-- changes to /scripts/ -->
+<script src='/media/aquissearch/js/responsiveCarousel.js'></script>
+<script src='/media/aquissearch/js/modernizr.custom.21882.js'></script>
+<script src='/media/COMMON/js/include-feed-3.0.js'></script>
+<script src='/media/aquissearch/js/jquery.simplyscroll.min.js'></script>
+
+<script src='/media/COMMON/js/responsive-plugins-0.js'></script>
+<script src='/media/aquissearch/js/myjs.js?v2'></script>
